c9ace5f5b9e49ef9a6388f7bb5cfa3c857d64077
IrynaZakharchenko <45123642+IrynaZakharchenko@users.noreply.github.com>
2019-01-22 02:53:09 +0200

    Initial commit

---
+++ calculator.cpp
@@ -0,0 +1,6 @@
+#include "calculator.hpp"
+
+double result(const std::string& eval)
+{
+    return 0;
+}
---
+++ calculator.hpp
@@ -0,0 +1,8 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <string>
+
+double result(const std::string& eval);
+
+#endif //CALCULATOR_H
